6d7bdb3195bf2cee0e7ea9825a77042ef2a5c9ce
Duncan Mac-Vicar P <dmacvicar@suse.de>
2011-03-22 13:29:00 +0100

    requires_eq and _ge are available only on Code11

--- libzypp.spec.cmake
+++ libzypp.spec.cmake
@@ -31,7 +31,7 @@ BuildRequires:  openssl-devel
 BuildRequires:  boost-devel dejagnu doxygen gcc-c++ gettext-devel graphviz hal-devel libxml2-devel
 
 BuildRequires:  libsatsolver-devel >= 0.14.13
-%if 0%{?suse_version}
+%if 0%{?suse_version} >= 1100
 %requires_eq    satsolver-tools
 %else
 Requires:	satsolver-tools
@@ -126,7 +126,7 @@ Requires:       libxml2-devel openssl-devel rpm-devel glibc-devel zlib-devel
 Requires:       bzip2 popt-devel dbus-1-devel glib2-devel hal-devel boost-devel libstdc++-devel
 Requires:       cmake
 Requires:       libcurl-devel >= %{min_curl_version}
-%if 0%{?suse_version}
+%if 0%{?suse_version} >= 1100
 %requires_ge    libsatsolver-devel
 %else
 Requires:	libsatsolver-devel

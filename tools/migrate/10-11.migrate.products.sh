294d3cfc6c7b4a9dae1c6c5089e4c1ea79d2c096
Michael Calmer <mc@suse.de>
2011-07-01 14:12:31 +0200

    implement product migration script

--- tools/migrate/10-11.migrate.products.sh
+++ tools/migrate/10-11.migrate.products.sh
@@ -9,5 +9,34 @@
 # - upon sucess mv /var/lib/zypp/db/products to products.migrated to
 #   prevent re-migration id bootstrap.sh is re-run.
 #
-echo "$0 NOT YET IMPLEMENTED" >&2
-exit 1
+SCHEMADIR=`dirname $0`
+OLD_ZYPP_DIR="/var/lib/zypp/db/products/"
+NEW_ZYPP_DIR="/etc/products.d/"
+
+mkdir -p $NEW_ZYPP_DIR
+BASE_INSTALL_TIME=2145999599
+
+for p in $OLD_ZYPP_DIR/*; do
+    #echo "$p"
+    NEW_PROD_NAME=`cat $p | sed -n 's/^.*<name>\(.\+\)<\/name>.*$/\1/p'`
+    NEW_PROD_NAME="$NEW_PROD_NAME.prod"
+    INSTALL_TIME=`cat $p | sed -n 's/^.*<install-time>\(.\+\)<\/install-time>.*$/\1/p'`
+    IS_BASE="no"
+    if grep '<product.*type="base' "$p" >/dev/null; then
+       IS_BASE="yes"
+    fi
+    /usr/bin/xsltproc --output $NEW_ZYPP_DIR/$NEW_PROD_NAME "$SCHEMADIR/10-11.migrate.product.xsl" $p
+    if [ "$IS_BASE" == "yes" ]; then
+       if [ -L "$NEW_ZYPP_DIR/baseproduct" -a $INSTALL_TIME -lt $BASE_INSTALL_TIME ]; then
+           #echo "found different baseproduct: $INSTALL_TIME < $BASE_INSTALL_TIME"
+           rm -f "$NEW_ZYPP_DIR/baseproduct"
+           ln -sf "$NEW_ZYPP_DIR/$NEW_PROD_NAME" "$NEW_ZYPP_DIR/baseproduct"
+           BASE_INSTALL_TIME=$INSTALL_TIME
+       elif [ ! -L "$NEW_ZYPP_DIR/baseproduct" ]; then
+           ln -sf "$NEW_ZYPP_DIR/$NEW_PROD_NAME" "$NEW_ZYPP_DIR/baseproduct"
+           BASE_INSTALL_TIME=$INSTALL_TIME
+       fi
+    fi
+done
+
+

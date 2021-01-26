e289ae0af74d63524b2f2a802428c5813481a2d3
Benjamin Zeller <bzeller@suse.de>
2021-01-26 14:05:49 +0100

    Introduce zypp-core library

---
+++ zypp-core/base/StringV.cc
@@ -0,0 +1,36 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+/** \file zypp/base/StringV.cc
+ */
+
+#include <zypp-core/base/StringV.h>
+
+///////////////////////////////////////////////////////////////////
+namespace zypp
+{
+  namespace strv
+  {
+
+    unsigned split( std::string_view line_r, std::string_view sep_r, Trim trim_r,
+		    std::function<void(std::string_view)> fnc_r )
+    {
+#warning REIMPLEMENT
+      std::vector<std::string> words;
+      str::split( std::string(line_r), std::back_inserter(words), std::string(sep_r), str::TRIM );
+
+      if ( fnc_r ) {
+	for ( const auto & w : words )
+	  fnc_r( std::string_view(w) );
+      }
+      return words.size();
+    }
+
+  } // namespace strv
+} // namespace zypp
+///////////////////////////////////////////////////////////////////

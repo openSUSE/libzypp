c3751ae173d2415cfb02ac157bc958baf968db13
Stefan Schubert <schubi@suse.de>
2022-06-20 11:59:06 +0200

    Removed %{_distconfdir}/logrotate.d from filelist. It will be supported by filesystem now.

--- libzypp.spec.cmake
+++ libzypp.spec.cmake
@@ -412,7 +412,6 @@ fi
 %config(noreplace) %{_sysconfdir}/zypp/zypp.conf
 %config(noreplace) %{_sysconfdir}/zypp/systemCheck
 %if 0%{?suse_version} > 1500
-%dir %{_distconfdir}/logrotate.d
 %config %{_distconfdir}/logrotate.d/zypp-history.lr
 %else
 %config(noreplace) %{_sysconfdir}/logrotate.d/zypp-history.lr

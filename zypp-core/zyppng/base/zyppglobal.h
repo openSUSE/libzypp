8580276031c59d8d31848a58b0cb0c2080e4d038
Benjamin Zeller <bzeller@suse.de>
2023-03-15 12:05:21 +0100

    Add helper macros for defining Class::create() functions and helpers

--- zypp-core/zyppng/base/zyppglobal.h
+++ zypp-core/zyppng/base/zyppglobal.h
@@ -104,13 +104,28 @@ template <typename Ptr> inline auto zyppGetPtrHelper(Ptr &ptr) -> decltype(ptr.o
 #define Z_D() auto const d = d_func()
 #define Z_Z() auto const z = z_func()
 
+/*!
+ * Helper macro to declare Ref types
+ */
+#define ZYPP_FWD_DECL_REFS(T) \
+  using T##Ref = std::shared_ptr<T>; \
+  using T##WeakRef = std::weak_ptr<T>
+
 /*
  * Helper Macro to forward declare types and ref types
  */
 #define ZYPP_FWD_DECL_TYPE_WITH_REFS(T) \
   class T; \
-  using T##Ref = std::shared_ptr<T>; \
-  using T##WeakRef = std::weak_ptr<T>
+  ZYPP_FWD_DECL_REFS(T)
+
+#define ZYPP_FWD_DECL_TEMPL_TYPE_WITH_REFS_ARG1(T, TArg1) \
+  template< typename TArg1> \
+  class T; \
+  template< typename TArg1> \
+  using T##Ref = std::shared_ptr<T<TArg1>>; \
+  template< typename TArg1> \
+  using T##WeakRef = std::weak_ptr<T<TArg1>>
+
 
 //@TODO enable for c++20
 #if 0
@@ -122,5 +137,81 @@ template <typename Ptr> inline auto zyppGetPtrHelper(Ptr &ptr) -> decltype(ptr.o
   template< typename TArg1 __VA_OPT__(, typename) __VA_ARGS__  > \
   using T##WeakRef = std::weak_ptr<T<TArg1 __VA_OPT__(,) __VA_ARGS__ >>
 #endif
+
+/*!
+ * Defines a dummy struct that can be used to make a public constructor unusable
+ * outside the class. \sa ZYPP_ADD_CREATE_FUNC.
+ */
+#define ZYPP_ADD_PRIVATE_CONSTR_HELPER() \
+  struct private_constr_t {  private_constr_t () noexcept = default; }
+
+/*!
+ * Use this to add the private constr argument to a constructor
+ */
+#define ZYPP_PRIVATE_CONSTR_ARG \
+  private_constr_t
+
+/*!
+ * Use this to pass the private constr arg to a constructor
+ */
+#define ZYPP_PRIVATE_CONSTR_ARG_VAL \
+  private_constr_t{}
+
+/*!
+ * Helper macro to add the default Class::create() static function
+ * commonly used in libzypp.
+ *
+ * \code
+ *
+ * // always forward declare the Ref types so they can be used in the class
+ * ZYPP_FWD_DECL_TYPE_WITH_REFS(MyClass)
+ *
+ * class MyClass
+ * {
+ *  // this adds the construct template function
+ *  ZYPP_ADD_CREATE_FUNC(MyClass)
+ *
+ *  public:
+ *    // adds a constructor that takes no args:
+ *    // MyClass::create();
+ *    ZYPP_DECL_PRIVATE_CONSTR(MyClass)
+ *
+ *    // adds a constructor with two arguments
+ *    // MyClass::create( 10, "Hello");
+ *    ZYPP_DECL_PRIVATE_CONSTR_ARGS(MyClass, int arg1, const std::string &label)
+ * }
+ *
+ * ZYPP_IMPL_PRIVATE_CONSTR( MyClass ) {
+ *  // do stuff
+ * }
+ *
+ * ZYPP_IMPL_PRIVATE_CONSTR_ARGS( MyClass, int arg1, const std::string &label ) {
+ *  // do stuff with args
+ * }
+ *
+ * \endcode
+ * \note requires the ref types for the class to be already defined
+ * \TODO Add this to already existing objects
+ */
+#define ZYPP_ADD_CREATE_FUNC(Class) \
+  private: \
+    ZYPP_ADD_PRIVATE_CONSTR_HELPER(); \
+  public: \
+    template < typename ...Args > \
+    inline static Class##Ref create ( Args &&... args ) { \
+      return std::make_shared< Class >( private_constr_t{}, std::forward<Args>(args)... ); \
+    } \
+  private:
+
+/*
+ * Convenience macros to implement public but private constructors that can be called from Class::create() but
+ * not by user code.
+ *
+ * \sa ZYPP_ADD_CONSTR_FUNC
+ */
+#define ZYPP_DECL_PRIVATE_CONSTR(Class) Class( private_constr_t )
+#define ZYPP_IMPL_PRIVATE_CONSTR(Class) Class::Class( private_constr_t )
+#define ZYPP_DECL_PRIVATE_CONSTR_ARGS(Class,...) Class( private_constr_t, __VA_ARGS__ )
+#define ZYPP_IMPL_PRIVATE_CONSTR_ARGS(Class,...) Class::Class( private_constr_t, __VA_ARGS__ )
 
 #endif

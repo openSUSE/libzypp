52637d64d4d4a1024d185d6da460ea7eddcad6c6
Benjamin Zeller <bzeller@suse.de>
2023-03-21 13:01:18 +0100

    Introduce UserInterface and UserRequest

---
+++ zypp-core/zyppng/ui/UserInterface
@@ -0,0 +1,1 @@
+#include "userinterface.h"
---
+++ zypp-core/zyppng/ui/userinterface.h
@@ -0,0 +1,48 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+#ifndef ZYPP_CORE_ZYPPNG_USERINTERFACE_H
+#define ZYPP_CORE_ZYPPNG_USERINTERFACE_H
+
+#include <zypp-core/zyppng/base/Base>
+#include <zypp-core/zyppng/base/Signals>
+
+namespace zyppng {
+
+  ZYPP_FWD_DECL_TYPE_WITH_REFS( UserInterface );
+  ZYPP_FWD_DECL_TYPE_WITH_REFS( UserRequest );
+
+  class UserInterfacePrivate;
+
+  /*!
+   * Generic code that allows libzypp to send messages and request
+   * directly to the user code.
+   *
+   * Every time a new request is sent the \ref UserInterface::sigEvent signal
+   * is emitted, which deliveres the specific request. Depending on the
+   * type of request the frontend code can return data to libzypp, for example
+   * if a user wants to trust a key, or a simple password request.
+   *
+   */
+  class UserInterface : public Base
+  {
+    ZYPP_DECLARE_PRIVATE(UserInterface)
+    ZYPP_ADD_CREATE_FUNC(UserInterface)
+
+  public:
+    ZYPP_DECL_PRIVATE_CONSTR(UserInterface);
+    void sendUserRequest( UserRequestRef event );
+    SignalProxy<void( UserRequestRef event)> sigEvent();
+
+  protected:
+    UserInterface( UserInterfacePrivate &d );
+  };
+
+}
+
+#endif

5cf7a61abc5e49c27deb80ddefe3e89fbaf7c29c
Michael Andres <ma@suse.de>
2023-05-03 14:13:29 +0200

    Update Specfile

--- libzypp.spec.cmake
+++ libzypp.spec.cmake
@@ -1,7 +1,7 @@
 #
 # spec file for package libzypp
 #
-# Copyright (c) 2005-2013 SUSE LINUX Products GmbH, Nuernberg, Germany.
+# Copyright (c) 2023 SUSE LLC
 #
 # All modifications and additions to the file contributed by third parties
 # remain the property of their copyright owners, unless otherwise agreed
@@ -12,7 +12,7 @@
 # license that conforms to the Open Source Definition (Version 1.9)
 # published by the Open Source Initiative.
 
-# Please submit bugfixes or comments via http://bugs.opensuse.org/
+# Please submit bugfixes or comments via https://bugs.opensuse.org/
 #
 
 
@@ -21,8 +21,8 @@
 Name:           @PACKAGE@
 Version:        @VERSION@
 Release:        0
-License:        GPL-2.0+
-Url:            git://gitorious.org/opensuse/libzypp.git
+License:        GPL-2.0-or-later
+URL:            https://github.com/openSUSE/libzypp
 Summary:        Package, Patch, Pattern, and Product Management
 Group:          System/Packages
 BuildRoot:      %{_tmppath}/%{name}-%{version}-build

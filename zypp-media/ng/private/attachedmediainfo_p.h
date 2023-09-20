f158b7448943514c499f7070fcff6733122d2d2a
Benjamin Zeller <bzeller@suse.de>
2023-09-20 17:11:53 +0200

    Refactor ProvideMediaHandle to use intrusive pointers

--- zypp-media/ng/private/attachedmediainfo_p.h
+++ zypp-media/ng/private/attachedmediainfo_p.h
@@ -16,52 +16,39 @@
 
 #include "providefwd_p.h"
 #include "providequeue_p.h"
+#include "zypp-core/base/ReferenceCounted.h"
 #include <zypp-media/ng/ProvideSpec>
 #include <string>
 #include <chrono>
+#include <optional>
 
 namespace zyppng {
 
   class ProvidePrivate;
 
-  struct AttachedMediaInfo {
+  DEFINE_PTR_TYPE(AttachedMediaInfo);
+
+  class AttachedMediaInfo : public zypp::base::ReferenceCounted, private zypp::base::NonCopyable {
+
+  protected:
+    void unref_to( unsigned int refCnt ) const override;
+    void ref_to( unsigned refCnt ) const override;
 
   public:
-    void ref() {
-      if ( _refCount == 0 )
-        _idleSince = std::chrono::steady_clock::time_point::max();
 
-      _refCount++;
-    }
-    void unref() {
-      if ( _refCount > 0 ) {
-        _refCount--;
-
-        if ( _refCount == 0 )
-          _idleSince = std::chrono::steady_clock::now();
-      }
-    }
+    AttachedMediaInfo( const std::string &name, ProvideQueueWeakRef backingQueue, ProvideQueue::Config::WorkerType workerType, const zypp::Url &baseUrl, const ProvideMediaSpec &mediaSpec );
 
     /*!
      * Returns true if \a other requests the same medium as this instance
      */
-    bool isSameMedium ( const std::vector<zypp::Url> &urls, const ProvideMediaSpec &spec ) {
-
-      const auto check = _spec.isSameMedium(spec);
-      if ( !zypp::indeterminate (check) )
-        return (bool)check;
-
-      // let the URL rule
-      return ( std::find( urls.begin(), urls.end(), _attachedUrl ) != urls.end() );
-    }
+    bool isSameMedium ( const std::vector<zypp::Url> &urls, const ProvideMediaSpec &spec );
 
     std::string _name;
     ProvideQueueWeakRef _backingQueue; //< if initialized contains a weak reference to the queue that owns this medium
     ProvideQueue::Config::WorkerType _workerType;
     zypp::Url   _attachedUrl; // the URL that was used for the attach request
     ProvideMediaSpec _spec;
-    uint _refCount = 0;
-    std::chrono::steady_clock::time_point _idleSince = std::chrono::steady_clock::time_point::max();
+    mutable std::optional<std::chrono::steady_clock::time_point> _idleSince; ///< Set if the medium is idle
   };
 
 }

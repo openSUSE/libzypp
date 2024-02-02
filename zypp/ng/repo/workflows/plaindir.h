4a49f768cb935dd080c81f3d72a3e26b200048f0
Benjamin Zeller <bzeller@suse.de>
2024-02-02 14:53:58 +0100

    Implement repo plaindir downloader as pipeline
    This patch duplicates the plaindirsusetags related repo downloader code as a pipeline.
    A subsequent commit will then switch to removing the old code and using the new one.

---
+++ zypp/ng/repo/workflows/plaindir.h
@@ -0,0 +1,37 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+#ifndef ZYPP_NG_PLAINDIR_WORKFLOW_INCLUDED
+#define ZYPP_NG_PLAINDIR_WORKFLOW_INCLUDED
+
+#include <zypp/RepoInfo.h>
+#include <zypp/ng/repo/Downloader>
+#include <zypp-core/zyppng/pipelines/AsyncResult>
+#include <zypp-core/zyppng/pipelines/Expected>
+
+#include <zypp/ManagedFile.h>
+
+
+namespace zyppng {
+  class ProvideMediaHandle;
+  class SyncMediaHandle;
+
+  ZYPP_FWD_DECL_TYPE_WITH_REFS( ProgressObserver );
+
+  namespace PlaindirWorkflows {
+    AsyncOpRef<expected<zypp::RepoStatus>> repoStatus( repo::AsyncDownloadContextRef dl, const ProvideMediaHandle &mediaHandle );
+    expected<zypp::RepoStatus> repoStatus( repo::SyncDownloadContextRef dl, const SyncMediaHandle &mediaHandle );
+
+    AsyncOpRef<expected<repo::AsyncDownloadContextRef>> download ( repo::AsyncDownloadContextRef dl, const ProvideMediaHandle &mediaHandle, ProgressObserverRef progressObserver = nullptr );
+    expected<repo::SyncDownloadContextRef> download ( repo::SyncDownloadContextRef dl, const SyncMediaHandle &mediaHandle, ProgressObserverRef progressObserver = nullptr );
+  }
+}
+
+
+
+#endif // ZYPP_NG_PLAINDIR_WORKFLOW_INCLUDED

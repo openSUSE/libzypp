943a2ba256631f7dd74649719d0f6561a1d40e43
Benjamin Zeller <bzeller@suse.de>
2024-02-02 14:52:00 +0100

    Implement susetags::Downloader as pipeline
    This patch duplicates the susetags related downloader code as a pipeline.
    A subsequent commit will then switch to removing the old code and using the new one.

---
+++ zypp/ng/repo/workflows/susetags.h
@@ -0,0 +1,49 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+#ifndef ZYPP_NG_SUSETAGS_WORKFLOW_INCLUDED
+#define ZYPP_NG_SUSETAGS_WORKFLOW_INCLUDED
+
+#include <zypp/RepoInfo.h>
+#include <zypp/ng/repo/Downloader>
+#include <zypp-core/zyppng/pipelines/AsyncResult>
+#include <zypp-core/zyppng/pipelines/Expected>
+
+#include <zypp/ManagedFile.h>
+
+
+namespace zyppng {
+  class ProvideMediaHandle;
+  class SyncMediaHandle;
+
+  ZYPP_FWD_DECL_TYPE_WITH_REFS( ProgressObserver );
+
+ /**
+  * \short Download workflow namespace for SUSETags (YaST2) repositories
+  * Encapsulates all the knowledge of which files have
+  * to be downloaded to the local disk and how to calculate the repo status.
+  */
+  namespace SuseTagsWorkflows {
+
+    /*!
+     * Calculate status of the remote SUSETags repository
+     */
+    AsyncOpRef<expected<zypp::RepoStatus>> repoStatus( repo::AsyncDownloadContextRef dl, const ProvideMediaHandle &mediaHandle );
+    expected<zypp::RepoStatus> repoStatus( repo::SyncDownloadContextRef dl, const SyncMediaHandle &mediaHandle );
+
+    /*!
+     * Download metadata to a local directory
+     */
+    AsyncOpRef<expected<repo::AsyncDownloadContextRef>> download ( repo::AsyncDownloadContextRef dl, const ProvideMediaHandle &mediaHandle, ProgressObserverRef progressObserver = nullptr );
+    expected<repo::SyncDownloadContextRef> download ( repo::SyncDownloadContextRef dl, const SyncMediaHandle &mediaHandle, ProgressObserverRef progressObserver = nullptr );
+  }
+}
+
+
+
+#endif // ZYPP_NG_SUSETAGS_WORKFLOW_INCLUDED

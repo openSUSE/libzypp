e414409cde635970675b2a0fa33ea56c3426f384
Christoph Grüninger <foss@grueninger.de>
2024-10-11 07:40:50 +0200

    Replace BOOST_STATIC_ASSERT and boost_is_integral by C++11's std

--- zypp/solver/detail/Resolver.cc
+++ zypp/solver/detail/Resolver.cc
@@ -18,7 +18,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
  * 02111-1307, USA.
  */
-#include <boost/static_assert.hpp>
 #include <utility>
 
 #define ZYPP_USE_RESOLVER_INTERNALS

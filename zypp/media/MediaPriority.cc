699b5029b23262b1172cf624a31227c27a12bf94
Christoph Grüninger <foss@grueninger.de>
2025-01-25 07:15:51 +0100

    MediaPriority: Replace clever but complicated case handling
    Group different cases by same return value, making it more
    readable. Performance should be not crucial in this case.
    
    Fixes a warning found by Clang (Wstring-plus-int).

--- zypp/media/MediaPriority.cc
+++ zypp/media/MediaPriority.cc
@@ -32,53 +32,44 @@ namespace zypp
 
       /**
        * 4: local:     file,dir,hd
-       * 3: remote:    nfs,cifs,smb
+       * 3: remote:    nfs,nfs4,cifs,smb
        * ?: download:  http,https,ftp,sftp, tftp
        * ?: volatile:  cd,dvd
        * 0:            the rest
       */
       MediaPriority::value_type scheme2priority(  const std::string & scheme_r )
       {
-        switch ( scheme_r[0] )
+        // local
+        if ( ( scheme_r.compare("file") == 0 )
+             || ( scheme_r.compare("dir") == 0 )
+             || ( scheme_r.compare("hd") == 0 ) )
         {
-#define RETURN_IF(scheme,value) \
-        if ( ::strcmp( scheme+1, scheme_r.c_str()+1 ) == 0 ) return value;
-          case 'c':
-            RETURN_IF( "cd",	ZConfig::instance().download_media_prefer_download() ? 1 : 2 );
-            RETURN_IF( "cifs",	3 );
-            break;
-
-          case 'd':
-            RETURN_IF( "dvd",	ZConfig::instance().download_media_prefer_download() ? 1 : 2 );
-            RETURN_IF( "dir",	4 );
-            break;
-
-          case 'f':
-            RETURN_IF( "file",	4 );
-            RETURN_IF( "ftp",	ZConfig::instance().download_media_prefer_download() ? 2 : 1);
-            break;
-
-          case 't':
-            RETURN_IF( "tftp",	ZConfig::instance().download_media_prefer_download() ? 2 : 1);
-            break;
-
-          case 'h':
-            RETURN_IF( "http",	ZConfig::instance().download_media_prefer_download() ? 2 : 1 );
-            RETURN_IF( "https",	ZConfig::instance().download_media_prefer_download() ? 2 : 1 );
-            RETURN_IF( "hd",	4 );
-            break;
-
-          case 'n':
-            RETURN_IF( "nfs",	3 );
-            RETURN_IF( "nfs4",	3 );
-            break;
-
-          case 's':
-            RETURN_IF( "sftp",	ZConfig::instance().download_media_prefer_download() ? 2 : 1 );
-            RETURN_IF( "smb",	3 );
-            break;
-#undef RETURN_IF
+          return 4;
         }
+        // remote
+        if ( ( scheme_r.compare("nfs") == 0 )
+             || ( scheme_r.compare("nfs4") == 0 )
+             || ( scheme_r.compare("cifs") == 0 )
+             || ( scheme_r.compare("smb") == 0 ) )
+        {
+          return 3;
+        }
+        // download
+        if ( ( scheme_r.compare("http") == 0 )
+             || ( scheme_r.compare("https") == 0 )
+             || ( scheme_r.compare("ftp") == 0 )
+             || ( scheme_r.compare("sftp") == 0 )
+             || ( scheme_r.compare("tftp") == 0 ) )
+        {
+          return ZConfig::instance().download_media_prefer_download() ? 2 : 1;
+        }
+        // volatile
+        if ( ( scheme_r.compare("cd") == 0 )
+             || ( scheme_r.compare("dvd") == 0 ) )
+        {
+          return ZConfig::instance().download_media_prefer_download() ? 1 : 2;
+        }
+        // the rest
         return 0;
       }
 

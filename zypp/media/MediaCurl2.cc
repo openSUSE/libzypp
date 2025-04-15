f70ef26ab8802bcfdef8109ecd1ff0c1278e83cf
Benjamin Zeller <bzeller@suse.de>
2025-04-15 13:28:58 +0200

    Introduce MediaNetworkRequestExecutor
    This patch factors the NetworkRequest execution part out of MediaCurl2
    into a small reusable class. For places where we need to simply request
    a Url but still want the MediaHandler callbacks to work.

--- zypp/media/MediaCurl2.cc
+++ zypp/media/MediaCurl2.cc
@@ -11,8 +11,6 @@
 */
 
 #include <iostream>
-#include <chrono>
-#include <list>
 
 #include <zypp/base/Logger.h>
 #include <zypp/ExternalProgram.h>
@@ -32,8 +30,6 @@
 #include <zypp/ZConfig.h>
 #include <zypp/zypp_detail/ZYppImpl.h> // for zypp_poll
 
-#include <zypp-core/zyppng/base/eventloop.h>
-#include <zypp-core/zyppng/base/private/threaddata_p.h>
 #include <zypp-curl/ng/network/networkrequestdispatcher.h>
 #include <zypp-curl/ng/network/request.h>
 
@@ -45,7 +41,7 @@
 #include <unistd.h>
 #include <glib.h>
 
-#include "detail/DownloadProgressTracker.h"
+#include "detail/MediaNetworkRequestExecutor.h"
 #include "detail/OptionalDownloadProgressReport.h"
 #include <zypp-curl/ng/network/private/mediadebug_p.h>
 
@@ -74,8 +70,7 @@ namespace zypp {
       : MediaNetworkCommonHandler( url_r, mirrors_r, attach_point_hint_r,
                                    "/", // urlpath at attachpoint
                                    true ) // does_download
-      , _evDispatcher( zyppng::ThreadData::current().ensureDispatcher() )
-      , _nwDispatcher( std::make_shared<zyppng::NetworkRequestDispatcher>() )
+      , _executor( std::make_shared<internal::MediaNetworkRequestExecutor>() )
     {
 
       MIL << "MediaCurl2::MediaCurl2(" << url_r.url() << ", " << attach_point_hint_r << ")" << endl;
@@ -261,8 +256,15 @@ namespace zypp {
         {
           // check if we can retry on the next mirror
           if( !canTryNextMirror ( excpt_r ) || ( mirr+1 >= _urls.size() ) ) {
+            // rewrite the exception to contain the correct pathname and url
+            // the executeRequest implementation just emits the full Url in the exception
+            if ( typeid(excpt_r) == typeid( MediaFileNotFoundException ) ) {
+              ZYPP_CAUGHT(excpt_r);
+              ZYPP_THROW( MediaFileNotFoundException( url(), filename ) );
+            }
             ZYPP_RETHROW(excpt_r);
           }
+          ZYPP_CAUGHT(excpt_r);
           continue;
         }
       }
@@ -398,142 +400,21 @@ namespace zypp {
 
     void MediaCurl2::executeRequest(  MediaCurl2::RequestData &reqData , callback::SendReport<DownloadProgressReport> *report )
     {
-      auto loop = zyppng::EventLoop::create();
-      auto &req = reqData._req;
-
-      _nwDispatcher->run();
-
-      bool firstAuth = true;
-      bool retry = true;
-      int  maxTries = req->transferSettings().maxSilentTries();
-
-      while ( retry ) {
-        std::optional<internal::ProgressTracker> progTracker;
-
-        std::vector<zyppng::connection> signalConnections {
-          req->sigStarted().connect( [&]( zyppng::NetworkRequest &req ){
-            if ( !report) return;
-            (*report)->start( req.url(), req.targetFilePath() );
-          }),
-          req->sigProgress().connect( [&]( zyppng::NetworkRequest &req, off_t dlTotal, off_t dlNow, off_t, off_t ){
-            if ( !report || !progTracker )
-              return;
-
-            progTracker->updateStats( dlTotal, dlNow );
-            if ( !(*report)->progress( progTracker->_dnlPercent, req.url(), progTracker-> _drateTotal, progTracker->_drateLast ) )
-              _nwDispatcher->cancel ( req );
-
-          }),
-          req->sigFinished().connect( [&]( zyppng::NetworkRequest &req, const zyppng::NetworkRequestError &err ) {
-            loop->quit();
-          })
-        };
-
-        // clean up slots for every loop
-        zypp_defer {
-          std::for_each( signalConnections.begin(), signalConnections.end(), []( auto &conn ) { conn.disconnect(); });
-          signalConnections.clear();
-        };
-
-        if ( report ) {
-          progTracker = internal::ProgressTracker();
+      const auto &authCb = [&]( const zypp::Url &, TransferSettings &settings, const std::string & availAuthTypes, bool firstTry, bool &canContinue ) {
+        if ( authenticate( _urls[reqData._mirrorIdx].url(), _mirrorSettings[reqData._mirrorIdx], availAuthTypes, firstTry ) ) {
+          settings = _mirrorSettings[reqData._mirrorIdx];
+          canContinue = true;
+          return;
         }
+        canContinue = false;
+      };
 
-        retry = false; // normally we don't retry
-        _nwDispatcher->enqueue ( req );
-        loop->run();
+      auto conn = _executor->sigAuthRequired().connect (authCb);
+      zypp_defer {
+        conn.disconnect ();
+      };
 
-        // once the request is done there should be nothing there anymore
-        if ( _nwDispatcher->count () != 0 ) {
-          ZYPP_THROW( zypp::Exception("Unexpected request count after finishing MediaCurl2 request!") );
-        }
-
-        if ( req->hasError() ) {
-          auto errCode = zypp::media::DownloadProgressReport::ERROR;
-          std::exception_ptr excp;
-          const auto &error = req->error();
-          switch ( error.type() ) {
-            case zyppng::NetworkRequestError::InternalError:
-            case zyppng::NetworkRequestError::InvalidChecksum:
-            case zyppng::NetworkRequestError::UnsupportedProtocol:
-            case zyppng::NetworkRequestError::MalformedURL:
-            case zyppng::NetworkRequestError::PeerCertificateInvalid:
-            case zyppng::NetworkRequestError::ConnectionFailed:
-            case zyppng::NetworkRequestError::ServerReturnedError:
-            case zyppng::NetworkRequestError::MissingData:
-            case zyppng::NetworkRequestError::RangeFail: {
-              excp = ZYPP_EXCPT_PTR( zypp::media::MediaCurlException( req->url(), error.toString(), error.nativeErrorString() ) );
-              break;
-            }
-            case zyppng::NetworkRequestError::Cancelled: {
-              excp = ZYPP_EXCPT_PTR( zypp::media::MediaRequestCancelledException( error.toString() ) );
-              break;
-            }
-            case zyppng::NetworkRequestError::ExceededMaxLen: {
-              excp = ZYPP_EXCPT_PTR( zypp::media::MediaFileSizeExceededException( req->url(), req->expectedFileSize() ) );
-              break;
-            }
-            case zyppng::NetworkRequestError::TemporaryProblem: {
-              excp = ZYPP_EXCPT_PTR( zypp::media::MediaTemporaryProblemException( req->url(), error.toString() ) );
-              break;
-            }
-            case zyppng::NetworkRequestError::Timeout: {
-              excp = ZYPP_EXCPT_PTR( zypp::media::MediaTimeoutException( req->url(), error.toString() ) );
-              break;
-            }
-            case zyppng::NetworkRequestError::Forbidden: {
-              excp = ZYPP_EXCPT_PTR( zypp::media::MediaForbiddenException( req->url(), error.toString() ) );
-              break;
-            }
-            case zyppng::NetworkRequestError::NotFound: {
-              errCode = zypp::media::DownloadProgressReport::NOT_FOUND;
-
-              //@BUG using getPathName() can result in wrong error messages
-              excp = ZYPP_EXCPT_PTR( zypp::media::MediaFileNotFoundException( _url.url(), req->url().getPathName() ) );
-              break;
-            }
-            case zyppng::NetworkRequestError::Unauthorized:
-            case zyppng::NetworkRequestError::AuthFailed: {
-
-              //in case we got a auth hint from the server the error object will contain it
-              std::string authHint = error.extraInfoValue("authHint", std::string());
-              if ( authenticate( _urls[reqData._mirrorIdx].url(), _mirrorSettings[reqData._mirrorIdx], authHint, firstAuth ) ) {
-                req->transferSettings() = _mirrorSettings[reqData._mirrorIdx];
-                firstAuth = false;
-                retry = true;
-                continue;
-              }
-
-              errCode = zypp::media::DownloadProgressReport::ACCESS_DENIED;
-              excp = ZYPP_EXCPT_PTR( zypp::media::MediaUnauthorizedException( req->url(), error.toString(), error.nativeErrorString(), "" ) );
-              break;
-            }
-            case zyppng::NetworkRequestError::NoError:
-              // should never happen
-              DBG << "BUG: Download error flag is set , but Error code is NoError" << std::endl;
-              break;
-            case zyppng::NetworkRequestError::Http2Error:
-            case zyppng::NetworkRequestError::Http2StreamError: {
-              excp = ZYPP_EXCPT_PTR( zypp::media::MediaCurlException( req->url(), error.toString(), error.nativeErrorString() ) );
-              break;
-            }
-          }
-
-          if ( excp ) {
-            if ( !retry && ( maxTries - 1 ) > 0 ) {
-              maxTries--;
-              retry = true;
-              continue;
-            }
-
-            if ( report ) (*report)->finish( req->url(), errCode, error.toString() );
-            std::rethrow_exception( excp );
-          }
-        }
-
-      }
-
-      if ( report ) (*report)->finish( req->url(), zypp::media::DownloadProgressReport::NO_ERROR, "" );
+      _executor->executeRequest ( reqData._req, report );
     }
   } // namespace media
 } // namespace zypp

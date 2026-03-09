5896df1043cc3bcdeb591d7d90bd0c33e0bb46a1
Benjamin Zeller <bzeller@suse.de>
2026-03-09 14:34:31 +0000

    Add NG wrapper headers for core types
    Add zyppng namespace wrapper headers that re-export the shared core
    types (Arch, Bit, CapMatch, CpeId, CountryCode, Dep, Edition, IdString,
    IdStringType, LanguageCode, Locale, Range, Rel, RelCompare, ResKind,
    ResTraits, ResolverNamespace) and base utilities (SerialNumber,
    SetTracker, StrMatcher) into the zyppng namespace via using declarations.
    
    These thin headers in zypp-logic/zypp/ng/ allow next-gen code to use
    zyppng::Arch, zyppng::Edition, etc. without directly depending on legacy
    zypp:: types.

---
+++ zypp-logic/zypp/ng/edition.h
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------\
+|                          ____ _   __ __ ___                          |
+|                         |__  / \ / / . \ . \                         |
+|                           / / \ V /|  _/  _/                         |
+|                          / /__ | | | | | |                           |
+|                         /_____||_| |_| |_|                           |
+|                                                                      |
+\---------------------------------------------------------------------*/
+// Automatically generated wrapper for zypp/Edition.h
+// Source Namespace: zypp -> Target Namespace: zyppng
+#ifndef EDITION_H_ZYPPNG_WRAPPER_H
+#define EDITION_H_ZYPPNG_WRAPPER_H
+
+#include <zypp/Edition.h>
+
+namespace zyppng
+{
+    using ::zypp::Edition;
+    using ::zypp::dumpAsXmlOn;
+} // namespace zyppng
+#endif // EDITION_H_ZYPPNG_WRAPPER_H

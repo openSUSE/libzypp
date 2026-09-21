8ae9cdefc72c6b44d8ca63ab26591d8658e2e05b
Michael Andres <ma@suse.de>
2026-09-21 17:23:15 +0200

    BuildRequires:  %{libsolv_devel_package} >= 0.7.40 (bsc#1279541)

--- zypp/libzypp.spec.cmake
+++ zypp/libzypp.spec.cmake
@@ -171,7 +171,7 @@ BuildRequires:  pkgconfig
 BuildRequires:  pkg-config
 %endif
 
-BuildRequires:  %{libsolv_devel_package} >= 0.7.34
+BuildRequires:  %{libsolv_devel_package} >= 0.7.40
 %if 0%{?suse_version} > 1500 || 0%{?sle_version} >= 150600
 BuildRequires:  libsolv-tools-base >= 0.7.29
 %requires_eq    libsolv-tools-base
